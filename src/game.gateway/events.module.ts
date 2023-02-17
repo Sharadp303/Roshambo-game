2c5538544f0cc7f7c4e999f84d6fb54a6bf80bc2
SHARAD PATEL <sharad.patels03@gmail.com>
2023-02-17 16:34:13 +0530

    winner

--- src/game.gateway/events.module.ts
+++ src/game.gateway/events.module.ts
@@ -1,8 +1,8 @@
 import { Module } from "@nestjs/common/";
 import { RoshamboGateway } from "./game.gateway";
-import { ChatGateway } from "./room.gateway";
+
 
 @Module({
-providers:[RoshamboGateway,ChatGateway]
+providers:[RoshamboGateway]
 })
 export class EventsModule{}
--- src/game.gateway/game.gateway.ts
+++ src/game.gateway/game.gateway.ts
@@ -22,55 +22,100 @@ export class RoshamboGateway implements OnGatewayInit, OnGatewayConnection,OnGat
 
 private players=[];
 private input=[];
-//private user=[]
+private user=[]
+private res=[]
 
                 @SubscribeMessage('game')
                 Play(client:Socket,move:String){
             
-                if(!this.players.includes(client.id))
-                {this.players.push(client.id)
-                console.log(this.players)}
+                if(!this.players.includes(client.id)){
+                    this.players.push(client.id)
+                   // console.log(this.players)
+                }
                 
                 
                 if(this.players.length<=1){   
                     this.input.push(move);
-                    //this.user.push({client:client.id,move,winning:0})
+                    this.user.push({client:client.id,move})
                     //console.log(this.user)
-                    console.log(move,this.input)
-                this.server.to(client.id).emit('rules',`Your move:${move} \n Waiting for ${2-(this.players.length)} more player`)
+                    console.log(client.id,move)
+                    this.server.to(client.id).emit('rules',`Your move:${move} \n Waiting for ${2-(this.players.length)} more player`)
                 }
+
 
                 if(this.players.length==2){
                     this.input.push(move);
-                    console.log(move,this.input)
+                    this.user.push({client:client.id,move})
+                    //console.log(this.user)
+                    console.log(client.id,move)
                     if(this.input.length==2){
                         if(this.input.includes("rock") && this.input.includes("paper")){
                             this.server.emit('results','Winner is Paper!!!!')
+                            const ans=this.user.find((ele)=>ele.move==='paper')
+                            this.res.push(ans.client)
                             this.input=[]
                         }
                         else if(this.input.includes("rock") && this.input.includes("scissor")){
                             this.server.emit('results','Winner is Rock!!!!')
+                            const ans=this.user.find((ele)=>ele.move==='rock')
+                            this.res.push(ans.client)
                             this.input=[]
                         }
                         else if(this.input.includes("paper") && this.input.includes("scissor")){
                             this.server.emit('results','Winner is Scissor!!!!')
+                            const ans=this.user.find((ele)=>ele.move==='scissor')
+                            this.res.push(ans.client)
                             this.input=[]
                         }
                         else{
                             this.server.emit('results','Its a TIE')
+                            
+                            this.res.push(0)
                             this.input=[]
                         }
                     }
                 }
-            
+            //console.log(this.res)
                     
                 if(this.players.length>=3){
                         this.server.to(client.id).emit('rules',`Room is full wait for some time to join or create a new room`)
                         this.players.pop();
                         console.log(this.players)
                     }
+                        if(this.res.length==3){
+                        const winner=this.findMaxRepeatedElement(this.res)
+                        this.server.emit('rules',`Winner is ${winner} ||||||!!!!! Hurray`)
+                        this.res=[];
+                        }
+
+            
+        }
+
+
+        private findMaxRepeatedElement(arr) {
+            let freq = {}; 
+            let maxFreq = 0; 
+            let ans="" 
+        
+            for (let i = 0; i < arr.length; i++) {
+            let elem = arr[i];
+            if (freq[elem]) {
+                freq[elem]++; 
+            } else {
+                freq[elem] = 1; 
             }
+            }
+            
+            for(let value in freq){
+            if(freq[value]>maxFreq){
+                maxFreq=freq[value]
+                ans=value
+            }
+                }
+        
+            return ans; 
+        }
+            
+    }
 
 
-
-}
