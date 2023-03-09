bc6dec4c2fb54828eecee71132ddf0012e9207b5
SHARAD PATEL <sharad.patels03@gmail.com>
2023-03-09 18:53:35 +0530

    multiple email

--- src/email/email.controller.ts
+++ src/email/email.controller.ts
@@ -1,5 +1,5 @@
 import { MailerService } from "@nestjs-modules/mailer";
-import { Controller, Get, Query } from "@nestjs/common";
+import { Body, Controller, Get, Post, Query } from "@nestjs/common";
 import { join } from "path";
 
 
@@ -21,6 +21,33 @@ async touser(@Query('toemail') toemail){
         }],
     })
     return 'success';
+}
+
+@Post()
+async multipleuser(@Body('email') email:string){
+    console.log(email)
+    let ans=email;
+  let newwmails= ans.split(",")
+
+    await newwmails.forEach(async (ele)=>{
+        await this.mailservice.sendMail({
+            to:ele,
+            from:'sharad.patels03@gmail.com',
+            subject:"testing mail service",
+            html:'<h1>Hii this is Sharad</h1>',
+            attachments:[{
+                path:join(__dirname,'Attachments','k.txt'),
+                filename:'1.txt',
+                contentDisposition:'attachment'
+            }],
+        })
+    })
+    
+    
+    
+     return "success";
+    
+
 }
 
 }
